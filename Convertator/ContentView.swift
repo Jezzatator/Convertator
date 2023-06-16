41171ed1b147096b8a5e4a2527389f339c5220b4
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-16 15:08:53 +0200

    Challenge fini peut améliorer la sélection de inputUnit == outputUnit

--- Convertator/ContentView.swift
+++ Convertator/ContentView.swift
@@ -10,11 +10,42 @@ import SwiftUI
 struct ContentView: View {
     
     @State var inputNumber = 0.0
-    @State var inputUnit: String = ""
-    @State var outputUnit: String = ""
+    @State var inputUnit: String = "°C"
+    @State var outputUnit: String = "°F"
     
     let unit = ["°C", "°F", "K"]
     
+// model
+    var Results : Double {
+        var results : Double = 0
+        // converti Celsius en Far
+        if inputUnit == "°C" && outputUnit == "°F"{
+            results = inputNumber * 9/5 + 32
+            return results
+        } // converti Celsius en Kelvin
+        else if inputUnit == "°C" && outputUnit == "K" {
+            results = inputNumber + 273.15
+            return results
+        } // Converti Far en Celsius
+        else if  inputUnit == "°F" && outputUnit == "°C" {
+            results = (inputNumber - 32) * (5/9)
+            return results
+        } // blabla
+        else if inputUnit == "°F" && outputUnit == "K" {
+            results = (inputNumber - 32) * (5/9) + 273.15
+            return results
+        } else if inputUnit == "K" && outputUnit == "°C" {
+            results = inputNumber - 273.15
+            return results
+        } else if inputUnit == "K" && outputUnit == "°F" {
+            results = (inputNumber - 273.15) * 9/5 + 32
+            return results
+        } else {
+            results = inputNumber
+            return results
+        }
+    }
+        
     var body: some View {
         NavigationView {
             Form {
@@ -31,7 +62,7 @@ struct ContentView: View {
                     Text("Inserez une valeur a convertir")
                 }
                 Section {
-                    Text(inputNumber, format: .number)
+                    Text(Results.formatted())
                     Picker("Unité", selection: $outputUnit) {
                         ForEach(unit, id: \.self) {
                             Text($0)
