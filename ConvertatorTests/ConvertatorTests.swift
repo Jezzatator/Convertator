9120eada54590783ab6567b7c8aa17017828e1dc
Jezzatator <jeremie.patot@protonmail.ch>
2023-08-03 11:50:27 +0200

    Test unitaire : ajouts de test pour les volumes et températures+ func conversion des volumes rendu flexible

--- ConvertatorTests/ConvertatorTests.swift
+++ ConvertatorTests/ConvertatorTests.swift
@@ -14,16 +14,21 @@ final class ConvertatorTests: XCTestCase {
     
     private var sutLong: Longueurs.ViewModel!
     private var sutTem: Temperature.ViewModel!
+    private var sutVol: Volumes.ViewModel!
     
     @MainActor override func setUpWithError() throws {
         sutLong = Longueurs.ViewModel()
         sutTem = Temperature.ViewModel()
+        sutVol = Volumes.ViewModel()
     }
     
     override func tearDownWithError() throws {
         sutLong = nil
         sutTem = nil
+        sutVol = nil
     }
+    
+    // UNIT TEST LONGUEUR
     
     @MainActor func test_convert10_return1093613LONG() {
         let actual = sutLong.ResultsLong(inputNumber: 10, inputUnit: "m", outputUnit: "yd")
@@ -60,6 +65,8 @@ final class ConvertatorTests: XCTestCase {
         XCTAssertEqual(actual, expected)
     }
     
+    // UNIT TEST TEMPERATURE
+    
     @MainActor func test_convert0C_return32FTEMP() {
         let actual = sutTem.ResultsTemp(inputNumber: 0.0 , inputUnit: "°C", outputUnit: "°F")
         let expected = 32.0
@@ -81,6 +88,21 @@ final class ConvertatorTests: XCTestCase {
         XCTAssertEqual(actual, expected)
     }
     
+    @MainActor func test_convert32C_return89FTEMP() {
+        let actual = sutTem.ResultsTemp(inputNumber: 32.0 , inputUnit: "°C", outputUnit: "°F")
+        let expected = 89.6
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    @MainActor func test_convert89F_return32FTEMP() {
+        let actual = sutTem.ResultsTemp(inputNumber: 89.6 , inputUnit: "°F", outputUnit: "°C")
+        let expected = 32.0
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    
     @MainActor func test_convert0K_returnNeg459FTEMP() {
         let actual = sutTem.ResultsTemp(inputNumber: 0.0 , inputUnit: "K", outputUnit: "°F")
         let expected = -459.66999999999996
@@ -90,9 +112,34 @@ final class ConvertatorTests: XCTestCase {
     
     @MainActor func test_convert0K_returnNeg273CTEMP() {
         let actual = sutTem.ResultsTemp(inputNumber: 0.0 , inputUnit: "K", outputUnit: "°C")
-        let expected = -273
+        let expected = -273.15
         
         XCTAssertEqual(actual, expected)
+    }
+    
+    // UNIT TEST VOLUME
+    //["m³", "dm³", "cm³", "l", "dl", "cl", "ml","fl oz UK", "fl oz US", "inch³", "ft³", "yard³", "gal UK", "gal US"]
+
+    @MainActor func test_convert1m_return1307yardVOL() {
+        let actual = sutVol.ResultsVol(inputNumber: 1.0 , inputUnit: "m³", outputUnit: "yard³")
+        let expected = 1.307950613786
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    @MainActor func test_convert43m_return1583ozUKVOL() {
+        let actual = sutVol.ResultsVol(inputNumber: 43.0 , inputUnit: "m³", outputUnit: "fl oz UK")
+        let expected = 1513388.4282977218
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    @MainActor func test_convert1000000000000l_return610237440947323inchVOL() {
+        let actual = sutVol.ResultsVol(inputNumber: 1000000000000.0, inputUnit: "l", outputUnit: "inch³")
+        let expected = 61023744094732.296875
+        
+        XCTAssertEqual(actual, expected)
+
     }
     
     func test_SampleTest() {
